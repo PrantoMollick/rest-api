5ca75a132dd88ab3bb01acbe29c49f89547e424c
Pranto Mollick <prantocse01@gmail.com>
2022-07-08 18:46:47 +0600

    intial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require("multer");
+const { v4 : uuidv4 } = require('uuid');
+
+require("dotenv").config();
+const DATABASE_URI = process.env.DATABASE_URI;
+
+
+const feedRoutes = require('./routes/feed');
+
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + '-' + file.originalname);
+  }
+})
+
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    return cb(null, true);
+  } else {
+    return cb(null, false);
+  }
+  cb(new Error('I don\'t have a clue!'))
+};
+
+const upload = multer({storage: storage, fileFilter: fileFilter}).single("image");
+// app.use(bodyParser.urlencoded()); //x-www-form-urlencoded default submitted throw form as post request
+app.use(bodyParser.json()); //application/json
+app.use(upload);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({
+    message: message
+  })
+});
+
+mongoose.connect(DATABASE_URI).catch(err => console.log(err));
+app.listen(8080)
