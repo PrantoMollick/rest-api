93bb91494717033387d43057636ee372f90f70ca
Pranto Mollick <prantocse01@gmail.com>
2022-07-09 18:51:10 +0600

    adding authorization checks

--- controllers/feed.js
+++ controllers/feed.js
@@ -145,6 +145,12 @@ exports.updatePost = (req, res, next) => {
         throw error;
       }
 
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
+        throw error;
+      }
+
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -176,6 +182,12 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error('Could not find post!');
         error.statusCode = 404;
+        throw error;
+      }
+      
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
         throw error;
       }
       //check logged in user
